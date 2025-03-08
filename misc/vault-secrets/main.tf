47f3e83c38d349c3e5c6e26b3ef5885db52c1320
vadlasaiteja-git <saitejav236@gmail.com>
2025-03-08 16:40:25 +0530

    Adding Shipping -Docker secrets

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -143,6 +143,11 @@ resource "vault_generic_secret" "roboshop-dev-shipping" {
 {
 "CART_ENDPOINT" : "cart-dev.saitejasroboshop.store:8080",
 "DB_HOST" : "mysql-dev.saitejasroboshop.store"
+"DB_TYPE" : "mysql",
+"APP_GIT_URL" : "https://github.com/roboshop-devops-project-v3/shipping",
+"DB_USER" : "root",
+"DB_PASS" : "RoboShop@1"
+
 }
 EOT
 }
