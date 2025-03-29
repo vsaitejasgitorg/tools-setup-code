994318f275c3b973ada6cbb291a0cd4748e40c27
vadlasaiteja-git <saitejav236@gmail.com>
2025-03-29 23:58:39 +0530

    Setting Up GitHub Runner

--- module-infra/iam.tf
+++ module-infra/iam.tf
@@ -37,3 +37,8 @@ resource "aws_iam_policy" "main" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
